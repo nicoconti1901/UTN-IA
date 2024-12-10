be8cf4e962b95abe86ce91d95d0a604b40b15d21
nicoconti1901 <nico.conti.1901@gmail.com>
2024-12-10 11:38:34 -0300

    creacion de la API a traves de express

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import idolos  from "./database/Idolos_de_River.js";
+
+// Crear el servidor de express
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Configurar CORS
+app.use(cors());
+
+// Rutas
+app.get("/", (req, res) => {
+  res.json(idolos);
+});
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
